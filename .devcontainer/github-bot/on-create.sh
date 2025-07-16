8cf78f34931dd87aaa4b01e077540863090133da
Daniel Gerlag <daniel@gerlag.ca>
2025-07-16 12:22:44 -0700

    GitHub Bot sample (#59)
    This pull request introduces a new GitHub bot tutorial by adding
    infrastructure, event sourcing, continuous queries, and reactions to
    automate issue tracking and labeling. The most important changes are
    grouped below:
    
    ### Infrastructure Setup:
    * Added an Azure Resource Manager (ARM) template
    (`azure-resources.json`) to provision resources, including a Function
    App, Event Hub, and Storage Account, to support the GitHub bot. The
    Function App processes GitHub events, and the Event Hub serves as the
    event source.
    
    ### Source:
    * Introduced a source configuration (`source.yaml`) to connect the bot
    to an Event Hub named `github` for consuming GitHub events.
    
    ### Continuous Queries:
    * Added `query-open-issues.yaml` to track and query open GitHub issues,
    inserting or deleting issue data based on "opened" or "closed" events.
    * Added `query-stale-issues.yaml` to identify stale issues by checking
    for inactivity (no comments or updates within a specified duration).
    
    ### Reactions:
    * Added `reaction-open-issues.yaml` to automatically comment on newly
    opened or closed issues, thanking contributors or notifying about
    closure.
    * Added `reaction-stale-issues.yaml` to label inactive issues as "stale"
    or remove the label when activity resumes.
    
    ---------
    
    Signed-off-by: Daniel Gerlag <daniel@gerlag.ca>

---
+++ .devcontainer/github-bot/on-create.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Copyright 2024 The Drasi Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Install kubectl
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+## Install Drasi CLI
+curl -fsSL https://raw.githubusercontent.com/drasi-project/drasi-platform/main/cli/installers/install-drasi-cli.sh | /bin/bash 
