3ec316b191e758df86bdf60a700e2be121e0aaf4
Jeffrey Zhang <jeffreyjpizhang@gmail.com>
2026-06-09 18:54:35 -0700

    chore: update workflow app with new drasi trigger interface
    Signed-off-by: Jeffrey Zhang <jeffreyjpizhang@gmail.com>

--- tutorial/dapr-agents/services/workflow/code/app.py
+++ tutorial/dapr-agents/services/workflow/code/app.py
@@ -4,21 +4,40 @@ import asyncio
 import logging
 
 from dapr_agents import AgentRunner
+from dapr_agents.agents.schemas import TriggerAction
 from dapr_agents.workflow.utils.core import wait_for_shutdown
 from dapr_agents.ext.drasi import drasi_trigger
 
 from agent import make_agent
-
+from models import DrasiUnpackedEvent
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+_DRASI_OP_TO_DESCRIPTION = {
+    "i": "insert",
+    "u": "update",
+    "d": "delete",
+    "x": "control",
+}
+
+
+def make_task(event: DrasiUnpackedEvent) -> TriggerAction:
+    return TriggerAction(
+        task=(
+            f"Create a summary of the changes in this {_DRASI_OP_TO_DESCRIPTION[event.op]} event for the '{event.payload.source.queryId}' query.\n"
+            f"This is the state before the change: {event.payload.before}.\n"
+            f"This is the state after the change: {event.payload.after}.\n"
+        )
+    )
 
 
 async def main() -> None:
     agent = make_agent()
 
     # Add Drasi CDC source
-    agent.add_activation(drasi_trigger())
+    drasi_trigger(agent, mapper=make_task)
 
     runner = AgentRunner()
     try:
